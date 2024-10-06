46fd89eb4cbb0c95a646aa79e09ecb578c452752
Hasith Udayanga <uhasith5@gmail.com>
2024-10-06 10:07:48 +0530

    fixes

--- resources/views/livewire/pages/components/card/related-section.blade.php
+++ resources/views/livewire/pages/components/card/related-section.blade.php
@@ -114,29 +114,32 @@ new class extends Component {
             </div>
             <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 my-12">
                 @foreach ($topCards as $card)
-                    <div class="flex w-full" wire:key="top-card-{{ $card->card_id }}">
-                        <div class="p-6 rounded-2xl bg-[#2C2C2C] bg-blend-screen">
-                            @if ($card?->all_card?->images['small'] !== null)
-                                <a href="{{ route('card-details', ['slug' => $card->slug, 'setSlug' => $card->set->slug]) }}"
-                                    wire:navigate>
-                                    <x-image :src="$card?->all_card?->images['small']" :alt="$card->name" skeltonWidth="180"
-                                        skeltonHeight="250" />
-                                </a>
-                            @else
-                                <a href="{{ route('card-details', ['slug' => $card->slug, 'setSlug' => $card->set->slug]) }}"
-                                    wire:navigate>
-                                    <div class="flex items-center justify-center bg-gray-300 rounded dark:bg-gray-700 animate-pulse"
-                                        style="width: 180px; height: 250px;">
-                                        <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                            <path
-                                                d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                                        </svg>
-                                    </div>
-                                </a>
-                            @endif
+                    @if ($card?->all_card?->images['small'] !== null)
+                        <div class="flex w-full" wire:key="top-card-{{ $card->card_id }}">
+                            <div class="p-6 rounded-2xl bg-[#2C2C2C] bg-blend-screen">
+                                @if ($card?->all_card?->images['small'] !== null)
+                                    <a href="{{ route('card-details', ['slug' => $card->slug, 'setSlug' => $card->set->slug]) }}"
+                                        wire:navigate>
+                                        <x-image :src="$card?->all_card?->images['small']" :alt="$card->name" skeltonWidth="180"
+                                            skeltonHeight="250" />
+                                    </a>
+                                @else
+                                    <a href="{{ route('card-details', ['slug' => $card->slug, 'setSlug' => $card->set->slug]) }}"
+                                        wire:navigate>
+                                        <div class="flex items-center justify-center bg-gray-300 rounded dark:bg-gray-700 animate-pulse"
+                                            style="width: 180px; height: 250px;">
+                                            <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                viewBox="0 0 20 18">
+                                                <path
+                                                    d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                                            </svg>
+                                        </div>
+                                    </a>
+                                @endif
+                            </div>
                         </div>
-                    </div>
+                    @endif
                 @endforeach
             </div>
         </div>
